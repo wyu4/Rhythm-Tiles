281585f7070daac385a8df8397d131169aa4a3ca
Wilson <wilsonyu657@gmail.com>
2024-06-06 13:07:19 -0400

    Added Maps static class and RESOURCES_DIR variable

--- src/Content/Resources.java
+++ src/Content/Resources.java
@@ -1,13 +1,25 @@
 package Content;
 
 public class Resources {
+    /*
+     * Storing this as a static variable so that directory location can be easily changed,
+     * without having to change all the locations inside the static classes below.
+     */
+    public static final String RESOURCES_DIR = "Resources";
+
+    // Static class containing relative locations of all images
     public static class Images {
-        public static final String BACK = "Resources\\Back.png";
-        public static final String EXIT = "Resources\\Exit.png";
-        public static final String PAUSE = "Resources\\Pause.png";
-        public static final String PLAY = "Resources\\Play.png";
-        public static final String RECORD = "Resources\\Record.png";
-        public static final String SAVE = "Resources\\Save.png";
-        public static final String RESTART = "Resources\\Restart.png";
+        public static final String BACK = RESOURCES_DIR + "\\Back.png";
+        public static final String EXIT = RESOURCES_DIR + "\\Exit.png";
+        public static final String PAUSE = RESOURCES_DIR + "\\Pause.png";
+        public static final String PLAY = RESOURCES_DIR + "\\Play.png";
+        public static final String RECORD = RESOURCES_DIR + "\\Record.png";
+        public static final String SAVE = RESOURCES_DIR + "\\Save.png";
+        public static final String RESTART = RESOURCES_DIR + "\\Restart.png";
+    }
+
+    // Static class containing relative locations of all maps
+    public static class Maps {
+        public static final String TEST = RESOURCES_DIR + "\\Test.json";
     }
 }
